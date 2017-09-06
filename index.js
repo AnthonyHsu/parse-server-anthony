e8f8be9d1056abb05ad3fe35547e613f6cd636f1
Anthony Hsu <janstin1122@gmail.com>
2017-09-06 00:11:14 -0700

    Update index.js

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: databaseUri || 'mongodb://janstin:mscsTrip690@ds062448.mlab.com:62448/tripsharing',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  appId: process.env.APP_ID || 'CfZTKqtflDp4wnQR5KtlmbVzk9coaFPz',
+  masterKey: process.env.MASTER_KEY || 'sKrVKQYdnw4HepWOiBbV1Qz8RrTjbEKN', //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || 'https://trip-sharing-mscs.herokuapp.com/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
